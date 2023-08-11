54ad5d5f6f928e0c4e11561b5e36805b63c37a81
Abhinav Jha <abhi.mount.26@gmail.com>
2023-08-11 14:15:36 +0530

    staging upload  url

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <NextScript />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(){var js,fs,d=document,id="tars-widget-script",b="https://tars-file-upload.s3.amazonaws.com/bulb/";if(!d.getElementById(id)){js=d.createElement("script");js.id=id;js.type="text/javascript";js.src=b+"js/widget.js";fs=d.getElementsByTagName("script")[0];fs.parentNode.insertBefore(js,fs)}})();window.tarsSettings = {"convid":"j9HpHV"};`
+            __html: `(function(){var js,fs,d=document,id="tars-widget-script",b="https://tars-file-upload-staging.s3.amazonaws.com/bulb/";if(!d.getElementById(id)){js=d.createElement("script");js.id=id;js.type="text/javascript";js.src=b+"js/widget.js";fs=d.getElementsByTagName("script")[0];fs.parentNode.insertBefore(js,fs)}})();window.tarsSettings = {"convid":"j9HpHV"};`
           }}
         />
       </body>
